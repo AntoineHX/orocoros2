e943a34e411d3ff7e706020fdf09e90197db9f23
Antoine H <antoine.harle@etu.upmc.fr>
2021-03-22 12:26:31 +0100

    Add first version of PongComp

---
+++ include/orocoros2/pongComp.hpp
@@ -0,0 +1,52 @@
+#ifndef OROCOROS2_PONGCOMP_HPP
+#define OROCOROS2_PONGCOMP_HPP
+
+#include <rtt/Component.hpp>
+#include <rtt/RTT.hpp>
+#include <rtt_ros2_services/rosservice.hpp>
+#include <rtt_ros2_topics/rostopic.hpp>
+#include <rtt_ros2_params/rosparam.hpp>
+#include <std_msgs/typekit/Types.hpp>
+
+namespace orocoros2
+{
+class pongComp : public RTT::TaskContext
+{
+  public:
+    explicit pongComp(const std::string& name);
+    virtual ~pongComp();
+
+    bool configureHook();
+    bool startHook();
+    void updateHook();
+    void stopHook();
+    void cleanupHook();
+
+    bool connect_to_topics_; //ROS Topic connection guard
+
+    float hit_chance_;
+    // int input_port_value_;
+    // int32_t event_port_value_;
+    // int32_t event_port_cb_value_;
+    // int32_t op_callback_value_;
+
+    // These ports are `int` ports, and will automatically map to `std_msgs/Int32` when connected to a ROS topic
+    // RTT::InputPort<std_msgs::msg::Int32> input_port_;
+    RTT::InputPort<bool> incoming_ball_port_;
+    RTT::OutputPort<bool> return_ball_port_;
+    RTT::OutputPort<bool> miss_ball_port_;
+    RTT::OutputPort<std_msgs::msg::Int32> player_action_port_;
+
+    // These ports are `std_msgs::Int32` to show that you can use ROS messages, too.
+    // RTT::InputPort<std_msgs::msg::Int32> event_port_;
+    // RTT::InputPort<std_msgs::msg::Int32> event_port_cb_;
+    // void eventPortCallback();
+
+    // Operations and OperationCallers that are tied to ROS services must share the same callback function definition as ROS services, though
+    // (but with non-const reference arguments, no shared pointers).
+    // RTT::OperationCaller<void(oe_msgs::srv::Operation::Request&, oe_msgs::srv::Operation::Response&)> op_caller_;
+    // void op_callback(oe_msgs::srv::Operation::Request&, oe_msgs::srv::Operation::Response&);
+};
+}  // namespace orocoros2
+
+#endif
---
+++ src/pongComp.cpp
@@ -0,0 +1,134 @@
+#include "orocoros2/pongComp.hpp"
+
+namespace orocoros2
+{
+pongComp::pongComp(const std::string& name)
+  : TaskContext(name)
+  , connect_to_topics_(false)
+  , hit_chance_(0.5)
+  // , input_port_value_(0)
+{
+
+  this->addProperty("connect_to_topics", connect_to_topics_).doc("ROS Topic connection guard");
+  this->addProperty("hit_chance", hit_chance_).doc("Hit Chance");
+  // this->addProperty("input_port_value", input_port_value_).doc("Latest value processed on pongComp Input Port");
+
+  //Orocos ports
+  this->ports()->addPort("incoming_ball_port", incoming_ball_port_).doc("pongComp Input Port");
+  this->ports()->addPort("return_ball_port", return_ball_port_).doc("pongComp Output Port");
+  this->ports()->addPort("miss_ball_port", miss_ball_port_).doc("pongComp Output Port");
+  //ROS ports
+  this->ports()->addPort("player_action_port", player_action_port_).doc("pongComp Output Port with ROS");
+
+  std::cout << "pongComp [" << getName() << "]: constructed" << std::endl;
+}
+
+pongComp::~pongComp()
+{
+  std::cout << "pongComp [" << getName() << "]: destruced" << std::endl;
+}
+
+bool pongComp::configureHook()
+{
+  bool success = true;
+
+  //ROS Params
+  boost::shared_ptr<rtt_ros2_params::RosParam> rosparam = this->getProvider<rtt_ros2_params::RosParam>("rosparam");
+  if (rosparam and rosparam->ready())
+  {
+    if (not rosparam->loadProperty("connect_to_topics", "connect_to_topics"))
+    {
+        std::cout << "pongComp [" << getName() << "]: failed to update rosparam [connect_to_topics]" << std::endl;
+    }
+    if (not rosparam->storeProperty("hit_chance", "hit_chance"))  // set() here to initialize rosparam value
+    {
+        std::cout << "pongComp [" << getName() << "]: failed to update rosparam [hit_chance]" << std::endl;
+    }
+  }
+  //ROS Topics
+  if (connect_to_topics_)
+  {
+    if (not player_action_port_.createStream(rtt_ros2_topics::topic("/" + getName() + "/player_action_port_")))
+    {
+        std::cout << "pongComp [" << getName() << "]: failed to connect to ROS topic [~/player_action_port_]" << std::endl;
+        success = false;
+    }
+  }
+  std::cout << "pongComp [" << getName() << "]: configured" << std::endl;
+  return success;
+}
+
+bool pongComp::startHook()
+{
+  bool success = true;
+
+  std::cout << "pongComp [" << getName() << "]: started" << std::endl;
+  return success;
+}
+
+void pongComp::updateHook()
+{
+  //Get latest value from rosparam
+  boost::shared_ptr<rtt_ros2_params::RosParam> rosparam = this->getProvider<rtt_ros2_params::RosParam>("rosparam");
+  if (rosparam and rosparam->ready())
+  {
+    if (not rosparam->loadProperty("hit_chance", "hit_chance"))
+    {
+        std::cout << "pongComp [" << getName() << "]: failed to update rosparam [hit_chance]" << std::endl;
+    }
+  }
+
+  // Process Inputs
+  //Orocos ports
+  bool input_temp;
+  // if (input_port_.read(input_temp) == RTT::FlowStatus::NewData)
+  // {
+  //     input_port_value_ = input_temp;
+  // }
+  incoming_ball_port_.read(input_temp);
+
+  // Do Something
+  std_msgs::msg::Int32 action;
+  action.data = 0; //Wait
+  bool miss=false;
+  if(input_temp)
+  {
+    action.data = 1; //Hit
+
+    miss = true;
+    action.data = 2; //Miss
+  }
+
+  // Process Outputs
+  //Orocos ports
+  if (input_temp && not return_ball_port_.write(!miss) == RTT::WriteStatus::WriteSuccess)
+  {
+      std::cerr << "pongComp [" << getName() << "]: return_ball_port failed to write" << std::endl;
+  }
+  if (not miss_ball_port_.write(miss) == RTT::WriteStatus::WriteSuccess)
+  {
+      std::cerr << "pongComp [" << getName() << "]: return_ball_port failed to write" << std::endl;
+  }
+  //ROS ports
+  if (not player_action_port_.write(action) == RTT::WriteStatus::WriteSuccess)
+  {
+      std::cerr << "pongComp [" << getName() << "]: player_action_port failed to write" << std::endl;
+  }
+
+  std::cout << "pongComp [" << getName() << "]: updated" << std::endl;
+}
+
+void pongComp::stopHook()
+{
+  std::cout << "pongComp [" << getName() << "]: stopped" << std::endl;
+}
+
+void pongComp::cleanupHook()
+{
+  std::cout << "pongComp [" << getName() << "]: cleaned" << std::endl;
+}
+
+
+}  // namespace orocoros2
+
+ORO_CREATE_COMPONENT(orocoros2::pongComp)
